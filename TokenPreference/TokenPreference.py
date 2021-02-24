0c6f5555c70590f7f69f6cce709d29b1d383749b
David Peng <david.peng050301@gmail.com>
2021-02-24 12:30:11 +1100

    Finalized helper for Token System Preferences

--- TokenPreference/TokenPreference.py
+++ TokenPreference/TokenPreference.py
@@ -18,6 +18,14 @@ while reader[i] != "Topics\n":
         for word in reader[i].split():
             if word.isdigit():
                 MaxSize = int(word)
+    if i == 2:
+        for word in reader[i].split():
+            if word.isdigit():
+                STDivisor = int(word)
+    if i == 3:
+        for word in reader[i].split():
+            if word.isdigit():
+                MinSize = int(word)
     #Add More Ifs here if there are more settings that need to be added
     i += 1
 
@@ -47,26 +55,46 @@ while reader[i] != "END\n":
     i += 1
 
 for Topic in range((len(students)//MaxSize) + 1):
+    print("=================================================================")
     x = PopularTopic(topics)
-    print(topics[x])
+    if topics[x][1] >= (len(students)/STDivisor) * len(topics):
+        print(topics[x], end = '')
+        print("- Consider Splitting into Subtopics")
+    else:
+        print(topics[x])
+    
     members = 0
+    member_list_short = []
     for it in range(len(topics)):
+        member_list_long = []
         for S in range(len(students)):
             if int(students[S][x + 1]) == len(topics) - it and StudentTeamStatus[students[S][0]] != "Has Team":
                 StudentTeamStatus.update({f"{students[S][0]}": "Has Team"})
                 print(f"{students[S][0]} : {students[S][x + 1]}")
+                member_list_long.append(S)
+                member_list_short.append(S)
                 members += 1
-            if members == MaxSize:
-                break;
         if members == MaxSize:
             break;
+    if members < MinSize:
+        print("\nConsider Reassigning People Below")
+        print("Their preferences are as follows")
+        for mem in member_list_short:
+            print(students[mem])
+
+    if members > MaxSize:
+        print("\nConsider Removing Some The Members Below")
+        print("Their preferences are as follows")
+        for mem in member_list_long:
+            print(students[mem])
     print("")
     topics[x][1] = 0
+    print("=================================================================")
 
 
-for Student in StudentTeamStatus:
-    if(StudentTeamStatus[Student] == "Has Team"):
-        print(Student + ' : ' + StudentTeamStatus[Student])
+#for Student in StudentTeamStatus:
+#    if(StudentTeamStatus[Student] == "Has Team"):
+#        print(Student + ' : ' + StudentTeamStatus[Student])
 
 for Student in StudentTeamStatus:
     if(StudentTeamStatus[Student] == "No Team"):
