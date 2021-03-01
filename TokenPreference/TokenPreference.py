722376a90854598d65b373961a6bb42e8e057cec
David Peng <david.peng050301@gmail.com>
2021-03-01 18:38:48 +1100

    Added in all the topics, also tuned the algorithm so that the program would not remove the teams under Min number unless there were no teams over the Max number

--- TokenPreference/TokenPreference.py
+++ TokenPreference/TokenPreference.py
@@ -2,7 +2,7 @@
 Max_Team_Size = 4
 Min_Team_Size = 2
 
-Topics = ["Machine Learning", "Organ Exchange", "Degree Planner", "Differential Privacy", "Random Sampling", "Visualization", "Benchmark Graph Instances"]
+Topics = ["Machine Learning", "Organ Exchange", "Degree Planner", "Differential Privacy", "Random Sampling", "Visualization", "Benchmark Graph Instances", "Graph Theory in Finance", "Enumeration Algorithms"]
 
 #Get Topics
 Topics_Membership = {
@@ -25,27 +25,27 @@ def add_student(Name, preferences, topics, student_Dictionary):
 
 
 Students = { }
-add_student("David Peng", [0, 0, 0, 7, 0, 0, 0], Topics, Students)
-add_student("Jodie Lee", [1, 1, 1, 1, 1, 1, 1], Topics, Students)
-add_student("Dallas Yan", [2, 0, 1, 2, 0, 1, 1], Topics, Students)
-add_student("Wisea Resosudarmo", [7, 0, 0, 0, 0, 0, 0], Topics, Students)
-add_student("Xinran Zhu", [3, 4, 0, 0, 0, 0, 0], Topics, Students)
-add_student("Angus Ritossa", [5, 0, 2, 0, 0, 0, 0], Topics, Students)
-add_student("Tiana Tsang Ung", [2, 2, 2, 1, 0, 0, 0], Topics, Students)
-add_student("Fergus Yip", [4, 3, 0, 0, 0, 0, 0], Topics, Students)
-add_student("Paula Tennet", [1, 1, 2, 0, 0, 3, 1], Topics, Students)
-add_student("Sarthak Sahoo", [3, 2, 0, 0, 2, 0, 0], Topics, Students)
-add_student("Yuanyuan Li", [3, 1, 1, 1, 1, 0, 0], Topics, Students)
-add_student("Diya Patel", [5, 2, 0, 0, 0, 0, 0], Topics, Students)
-add_student("Aksay Ram Moham Valluru", [0, 0, 0, 3, 4, 0, 0], Topics, Students)
-add_student("Cece Zhu", [2, 0, 0, 4, 0, 1, 0], Topics, Students)
-add_student("Peter Derias", [2, 2, 0, 2, 1, 0, 0], Topics, Students)
-add_student("Pengsen Mao", [0, 0, 4, 0, 0, 2, 1], Topics, Students)
-add_student("Andrew Xie", [3, 0, 3, 0, 1, 0, 0], Topics, Students)
-add_student("Joshua Lam", [3, 0, 0, 0, 0, 4, 0], Topics, Students)
-add_student("Shirley Zhou", [0, 0, 0, 0, 5, 1, 1], Topics, Students)
-add_student("Sachin Paliwal", [0, 0, 3, 0, 2, 0, 2], Topics, Students)
-add_student("Nian Li", [2, 0, 3, 0, 2, 0, 0], Topics, Students)
+add_student("David Peng", [3, 2, 1, 0, 2, 0, 0, 0, 0], Topics, Students)
+add_student("Jodie Lee", [1, 1, 1, 1, 3, 0, 0, 0, 0], Topics, Students)
+add_student("Dallas Yan", [2, 0, 1, 2, 0, 1, 1, 0, 0], Topics, Students)
+add_student("Wisea Resosudarmo", [3, 2, 2, 1, 0, 0, 0, 0, 0], Topics, Students)
+add_student("Xinran Zhu", [3, 2, 1, 0, 1, 0, 0, 0, 0], Topics, Students)
+add_student("Angus Ritossa", [4, 0, 2, 0, 1, 0, 0, 0, 0], Topics, Students)
+add_student("Tiana Tsang Ung", [2, 2, 2, 1, 0, 0, 0, 0, 0], Topics, Students)
+add_student("Fergus Yip", [2, 3, 0, 0, 0, 2, 0, 0, 0], Topics, Students)
+add_student("Paula Tennet", [1, 1, 2, 0, 0, 3, 1, 0, 0], Topics, Students)
+add_student("Sarthak Sahoo", [3, 2, 0, 0, 2, 0, 0, 0, 0], Topics, Students)
+add_student("Yuanyuan Li", [3, 1, 1, 1, 1, 0, 0, 0, 0], Topics, Students)
+add_student("Diya Patel", [3, 2, 0, 1, 0, 1, 0, 0, 0], Topics, Students)
+add_student("Aksay Ram Moham Valluru", [0, 2, 0, 3, 2, 0, 0, 0, 0], Topics, Students)
+add_student("Cece Zhu", [2, 0, 1, 3, 0, 1, 0, 0, 0], Topics, Students)
+add_student("Peter Derias", [2, 2, 0, 2, 1, 0, 0, 0, 0], Topics, Students)
+add_student("Pengsen Mao", [0, 0, 2, 2, 0, 2, 1, 0, 0], Topics, Students)
+add_student("Andrew Xie", [3, 0, 3, 0, 1, 0, 0, 0, 0], Topics, Students)
+add_student("Joshua Lam", [3, 0, 0, 1, 0, 3, 0, 0, 0], Topics, Students)
+add_student("Shirley Zhou", [0, 3, 0, 0, 2, 1, 1, 0, 0], Topics, Students)
+add_student("Sachin Paliwal", [0, 0, 3, 0, 2, 0, 2, 0, 0], Topics, Students)
+add_student("Nian Li", [2, 0, 3, 0, 2, 0, 0, 0, 0], Topics, Students)
 
 #Get each student's highest preference
 def Membership_Division(Topics_Membership, Student_List):
@@ -86,17 +86,20 @@ def Remove_Lowest(Topic_Members, No_Team_Students):
 
 z = 0
 while len(No_Team_Students) > 0:
+    Remove_Min = True
     result = Membership_Division(Topics_Membership, No_Team_Students)
     if result == False:
         break
     for Topic in list(Topics_Membership.keys()):
         if len(Topics_Membership[Topic]) > Max_Team_Size:
+            Remove_Min = False
             for i in range(len(Topics_Membership[Topic]) - Max_Team_Size):
                 Remove_Lowest(Topics_Membership[Topic], No_Team_Students)
-    for Topic in list(Topics_Membership.keys()):
-        if len(Topics_Membership[Topic]) < Min_Team_Size:
-            for i in range(len(Topics_Membership[Topic])):
-                Remove_Lowest(Topics_Membership[Topic], No_Team_Students)
+    if Remove_Min == True:
+        for Topic in list(Topics_Membership.keys()):
+            if len(Topics_Membership[Topic]) < Min_Team_Size:
+                for i in range(len(Topics_Membership[Topic])):
+                    Remove_Lowest(Topics_Membership[Topic], No_Team_Students)
     
 
 for topic in Topics:
